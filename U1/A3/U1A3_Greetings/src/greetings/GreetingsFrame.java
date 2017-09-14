cdd768fc3564915d3369018b380728840f4b5265
Meep3692@gmail.com <>
2017-09-14 13:22:46 -0400

    U1A3: IT WORKS! Don't touch it, it works!

--- U1/A3/U1A3_Greetings/src/greetings/GreetingsFrame.java
+++ U1/A3/U1A3_Greetings/src/greetings/GreetingsFrame.java
@@ -5,6 +5,7 @@
  */
 package greetings;
 
+import java.awt.*;
 import javax.swing.*;
 
 /**
@@ -12,12 +13,16 @@ import javax.swing.*;
  * @author awoo
  */
 public class GreetingsFrame extends JFrame {
+    
+    private String text = "My name is Darian, I go to North Dundas. I listen to a lot of 80s music. Recently, I found a show called \"Squirrel and Hedgehog\" which is a North Korean animated tv series that, although mostly being North Korean military propeganda, is a prety good show. My favorite colour is an agressive shade of mauve because mauve is normally such a peaceful and innocent colour.";
+    
     public GreetingsFrame(){
-        super("Hello!");
+        super("About Me");
         setSize(500,200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("<html>" + text + "</html>");
+        //Leaving it as default layout because it works for this situation
+        add(label);
         setVisible(true);
-        JLabel label = new JLabel("Hello!");
-        
     }
 }
