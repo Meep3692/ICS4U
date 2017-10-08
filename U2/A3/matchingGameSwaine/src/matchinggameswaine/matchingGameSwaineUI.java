3a21932843fea0cde22b6f34b79f9c8027ecc65e
17752775+Meep3692@users.noreply.github.com <17752775+Meep3692@users.noreply.github.com>
2017-10-08 18:37:46 -0400

    U2A3: Added victory, added support for custom themes, added auto reguess

--- U2/A3/matchingGameSwaine/src/matchinggameswaine/matchingGameSwaineUI.java
+++ U2/A3/matchingGameSwaine/src/matchinggameswaine/matchingGameSwaineUI.java
@@ -8,9 +8,14 @@ package matchinggameswaine;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import javax.swing.ImageIcon;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 /**
  *
  * @author jonswaine
@@ -23,12 +28,9 @@ public class matchingGameSwaineUI extends javax.swing.JFrame implements ActionLi
     ArrayList<Integer> set = new ArrayList();
     //Store buttons
     ArrayList<JButton> buttons = new ArrayList();
-    //Store card images
-    ArrayList<ImageIcon> icons = new ArrayList();
+    //Current theme
+    Theme theme;
     
-    //Icons for back of card and pair
-    ImageIcon back = new ImageIcon(getClass().getResource("/matchinggameswaine/cardback.jpg"));
-    ImageIcon done = new ImageIcon(getClass().getResource("/matchinggameswaine/done.jpg"));
     //Count: How many cards are flipped
     //c1, c2 first and second card flipped
     //cardCount is the number of cards
@@ -43,15 +45,13 @@ public class matchingGameSwaineUI extends javax.swing.JFrame implements ActionLi
      */
     public matchingGameSwaineUI() {
         initComponents();
-        //Add icons
-        icons.add(new ImageIcon(getClass().getResource("/matchinggameswaine/ram.jpg")));
-        icons.add(new ImageIcon(getClass().getResource("/matchinggameswaine/case.jpg")));
-        icons.add(new ImageIcon(getClass().getResource("/matchinggameswaine/dvd.jpg")));
-        icons.add(new ImageIcon(getClass().getResource("/matchinggameswaine/harddrive.jpg")));
-        icons.add(new ImageIcon(getClass().getResource("/matchinggameswaine/keyboard.jpg")));
-        icons.add(new ImageIcon(getClass().getResource("/matchinggameswaine/mice.jpg")));
-        icons.add(new ImageIcon(getClass().getResource("/matchinggameswaine/monitor.jpg")));
-        icons.add(new ImageIcon(getClass().getResource("/matchinggameswaine/printer.jpg")));
+        try {
+            //Add icons
+            theme = Theme.loadTheme("ReZeroTheme.zip");//Defaulting to this to make it more obvious
+        } catch (IOException ex) {
+            Logger.getLogger(matchingGameSwaineUI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        setSize(800, 600);//Set size to not be the size of very small
     }
     
     private int[] makeSquarish(int count){
@@ -74,15 +74,43 @@ public class matchingGameSwaineUI extends javax.swing.JFrame implements ActionLi
     private void initComponents() {
         java.awt.GridBagConstraints gridBagConstraints;
 
+        victoryDialog = new javax.swing.JDialog();
+        victoryButtonsPanel = new javax.swing.JPanel();
+        victoryOkButton = new javax.swing.JButton();
+        victoryText = new javax.swing.JLabel();
+        fileChooser = new javax.swing.JFileChooser();
         titleLabel = new javax.swing.JLabel();
         buttonsPanel = new javax.swing.JPanel();
         controlsPanel = new javax.swing.JPanel();
         btnPlay = new javax.swing.JButton();
         cardCountField = new javax.swing.JTextField();
-        btnGuessAgain = new javax.swing.JButton();
         btnExit = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        menuBar = new javax.swing.JMenuBar();
+        fileMenu = new javax.swing.JMenu();
+        themeMenuItem = new javax.swing.JMenuItem();
+
+        victoryDialog.setTitle("Congratulations");
+
+        victoryButtonsPanel.setLayout(new java.awt.GridBagLayout());
+
+        victoryOkButton.setText("Ok");
+        victoryOkButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPlayActionPerformed(evt);
+            }
+        });
+        victoryButtonsPanel.add(victoryOkButton, new java.awt.GridBagConstraints());
+
+        victoryDialog.getContentPane().add(victoryButtonsPanel, java.awt.BorderLayout.PAGE_END);
+
+        victoryText.setText("Congratulations, you won!");
+        victoryDialog.getContentPane().add(victoryText, java.awt.BorderLayout.CENTER);
+
+        fileChooser.setDialogTitle("Open");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Match Game");
 
         titleLabel.setFont(new java.awt.Font("Big Caslon", 0, 36)); // NOI18N
         titleLabel.setForeground(new java.awt.Color(255, 51, 51));
@@ -103,26 +131,13 @@ public class matchingGameSwaineUI extends javax.swing.JFrame implements ActionLi
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridheight = 2;
         controlsPanel.add(btnPlay, gridBagConstraints);
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         controlsPanel.add(cardCountField, gridBagConstraints);
-
-        btnGuessAgain.setText("Guess Again");
-        btnGuessAgain.setActionCommand("");
-        btnGuessAgain.setAutoscrolls(true);
-        btnGuessAgain.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnGuessAgainActionPerformed(evt);
-            }
-        });
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridheight = 2;
-        controlsPanel.add(btnGuessAgain, gridBagConstraints);
 
         btnExit.setText("Exit");
         btnExit.setActionCommand("");
@@ -133,11 +148,32 @@ public class matchingGameSwaineUI extends javax.swing.JFrame implements ActionLi
             }
         });
         gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.gridheight = 2;
         controlsPanel.add(btnExit, gridBagConstraints);
 
+        jLabel1.setText("Cards:");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        controlsPanel.add(jLabel1, gridBagConstraints);
+
         getContentPane().add(controlsPanel, java.awt.BorderLayout.SOUTH);
+
+        fileMenu.setText("File");
+
+        themeMenuItem.setText("Load Theme");
+        themeMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                themeMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(themeMenuItem);
+
+        menuBar.add(fileMenu);
+
+        setJMenuBar(menuBar);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -148,19 +184,17 @@ public class matchingGameSwaineUI extends javax.swing.JFrame implements ActionLi
 
     //Hit play button
     private void btnPlayActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPlayActionPerformed
+        victoryDialog.setVisible(false);
         buttonsPanel.removeAll();//Reset buttons
         buttons.clear();
+        set.clear();//Clear card set
+        cards.clear();//Clear cards
         try{//Parse card count or default to 16
             cardCount = Integer.parseInt(cardCountField.getText());
         }catch(NumberFormatException e){
             cardCount = 16;
         }
         cardsLeft = cardCount;
-        
-        GridLayout layout = (GridLayout)buttonsPanel.getLayout();//Get layout
-        int[] dimensions = makeSquarish(cardCount);//Make roughly square
-        layout.setRows(dimensions[0]);
-        layout.setColumns(dimensions[1]);
         
         change = new int[cardCount];//Reset state array size
         //Reset all cards to default state
@@ -169,9 +203,14 @@ public class matchingGameSwaineUI extends javax.swing.JFrame implements ActionLi
             change[z] = 1;
         }
         
+        int cardSetSize = theme.cardFaces.size();
+        if(cardCount < cardSetSize * 2){
+            cardSetSize = cardCount / 2;
+        }
+        System.out.println(cardSetSize);
         //Add every card type to arraylist twice for as many cards as we need
         while(set.size() < cardCount)
-            for (int x=0; x <= 7; x++)
+            for (int x=0; x < cardSetSize; x++)
             {
                 set.add(x);
                 set.add(x);
@@ -187,39 +226,33 @@ public class matchingGameSwaineUI extends javax.swing.JFrame implements ActionLi
             change[x] = 1;//reset card state
             
             JButton button = new JButton();//New card button
-            button.setIcon(back);//Set icon to default
+            button.setIcon(theme.back);//Set icon to default
             button.setName("guessButton" + x);//Set name for later identification
             button.addActionListener(this);//Set action listener to this class
             buttonsPanel.add(button, x);//Add to panel
             buttons.add(button);//Add to list
         }
-        buttonsPanel.repaint();//Repaint panel so buttons show
+        GridLayout layout = (GridLayout)buttonsPanel.getLayout();//Get layout
+        int[] dimensions = makeSquarish(cardCount);//Make roughly square
+        layout.setRows(dimensions[0]);
+        layout.setColumns(dimensions[1]);
+        
+        invalidate();//Stuff for repainting
+        validate();
+        repaint();//Repaint panel so buttons show
     }//GEN-LAST:event_btnPlayActionPerformed
 
-    private void btnGuessAgainActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuessAgainActionPerformed
-        
-        count = 0;//Reset count
-        if (c1 == c2)//Check cards are the same
-        {
-            for(int i = 0; i < cardCount; i++){//Set cards to finished
-                if(change[i] == 0){//Card is flipped
-                    buttons.get(i).setIcon(done);
-                    change[i] = 2;
-                }
-            }
-            cardsLeft -= 2;//Subtract from cards left
-        } 
-        else 
-        {
-            for(int i = 0; i < cardCount; i++){//Reset cards
-                if(change[i] == 0){//Card is flipped
-                    buttons.get(i).setIcon(back);
-                    change[i] = 1;
-                }
+    private void themeMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_themeMenuItemActionPerformed
+        int response = fileChooser.showOpenDialog(this);
+        if(response == JFileChooser.APPROVE_OPTION){
+            File file = fileChooser.getSelectedFile();
+            try {
+                theme = Theme.loadTheme(file.getAbsolutePath());
+            } catch (IOException ex) {
+                Logger.getLogger(matchingGameSwaineUI.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        
-    }//GEN-LAST:event_btnGuessAgainActionPerformed
+    }//GEN-LAST:event_themeMenuItemActionPerformed
 
     /**
      * @param args the command line arguments
@@ -248,6 +281,9 @@ public class matchingGameSwaineUI extends javax.swing.JFrame implements ActionLi
         }
         //</editor-fold>
 
+        System.out.println("Working Directory = " +
+              System.getProperty("user.dir"));
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -258,23 +294,31 @@ public class matchingGameSwaineUI extends javax.swing.JFrame implements ActionLi
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnExit;
-    private javax.swing.JButton btnGuessAgain;
     private javax.swing.JButton btnPlay;
     private javax.swing.JPanel buttonsPanel;
     private javax.swing.JTextField cardCountField;
     private javax.swing.JPanel controlsPanel;
+    private javax.swing.JFileChooser fileChooser;
+    private javax.swing.JMenu fileMenu;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JMenuItem themeMenuItem;
     private javax.swing.JLabel titleLabel;
+    private javax.swing.JPanel victoryButtonsPanel;
+    private javax.swing.JDialog victoryDialog;
+    private javax.swing.JButton victoryOkButton;
+    private javax.swing.JLabel victoryText;
     // End of variables declaration//GEN-END:variables
 
     //Button pressed
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton button = (JButton)e.getSource();
-        if(button.getName().startsWith("guessButton")){
+        if(button.getName().startsWith("guessButton") && count < 2){//Are we a card and are there less than 2 cards flipped
             int buttonID = Integer.parseInt(button.getName().substring(11));//Parse id from name
             if(change[buttonID] == 1){//Are we flipped on our back
                 int cardType = cards.get(buttonID);//Get type of current card
-                button.setIcon(icons.get(cardType));//Set icon to our card
+                button.setIcon(theme.cardFaces.get(cardType));//Set icon to our card
                 change[buttonID] = 0;//Set state of card to flipped
                 count++;//Add to cards flipped counter
                 
@@ -282,8 +326,44 @@ public class matchingGameSwaineUI extends javax.swing.JFrame implements ActionLi
                     c1 = cardType;
                 }else if(count == 2){
                     c2 = cardType;
+                    new Thread(() -> {
+                        cardsFlipped();
+                    }).start();
                 }
             }
+        }
+    }
+    
+    private void cardsFlipped(){
+        try {
+            Thread.sleep(1000);//Wait for a second
+            count = 0;//Reset count
+            if (c1 == c2)//Check cards are the same
+            {
+                for(int i = 0; i < cardCount; i++){//Set cards to finished
+                    if(change[i] == 0){//Card is flipped
+                        buttons.get(i).setIcon(theme.done);
+                        change[i] = 2;
+                    }
+                }
+                cardsLeft -= 2;//Subtract from cards left
+                if(cardsLeft < 1){
+                    victoryDialog.setSize(200, 100);
+                    victoryDialog.setVisible(true);
+                }
+            } 
+            else 
+            {
+                for(int i = 0; i < cardCount; i++){//Reset cards
+                    if(change[i] == 0){//Card is flipped
+                        buttons.get(i).setIcon(theme.back);
+                        change[i] = 1;
+                    }
+                }
+            }
+        } catch (InterruptedException ex) {
+            //This should never happen
+            Logger.getLogger(matchingGameSwaineUI.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
