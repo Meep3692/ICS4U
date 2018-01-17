68078d0dcd3c18ec48406408d164e235ea6902db
Meep3692 <Meep3692@gmail.comL>
2018-01-17 17:31:12 -0500

    July Fight: Slick2D working

--- U4/JulyFight/src/julyfight/JulyFight.java
+++ U4/JulyFight/src/julyfight/JulyFight.java
@@ -5,17 +5,53 @@
  */
 package julyfight;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
 /**
  *
  * @author Awoo
  */
-public class JulyFight {
+public class JulyFight extends BasicGame {
 
+    public JulyFight(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer container) throws SlickException {
+        
+    }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+        
+    }
+
+    @Override
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        try {
+            JulyFight game = new JulyFight("Awoo");
+            AppGameContainer gc = new AppGameContainer(game);
+            gc.setDisplayMode(800, 600, false);
+            gc.start();
+        } catch (SlickException ex) {
+            Logger.getLogger(JulyFight.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
